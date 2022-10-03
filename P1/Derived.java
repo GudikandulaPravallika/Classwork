9e552383c033f6df53c00f776b41c5734a2f0754
GudikandulaPravallika <21bq1a4250@vvit.net>
2022-10-03 14:01:48 +0530

    Updated Derived file

--- P1/Derived.java
+++ P1/Derived.java
@@ -3,6 +3,7 @@ package P1;
 public class Derived extends Protection {
 	public Derived()
 	{
+            System.out.println("Access Modifiers");
 		System.out.println("base constructor");
 		//System.out.println("n_pri"+n_pri);
 		System.out.println("n_public"+n_pub);
