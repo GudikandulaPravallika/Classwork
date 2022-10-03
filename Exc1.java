e720b582f0efbe1833a870f4ee20950914239e79
GudikandulaPravallika <21bq1a4250@vvit.net>
2022-10-03 14:17:23 +0530

    Exception Handling

---
+++ Exc1.java
@@ -0,0 +1,15 @@
+
+public class Exc1 {
+	static void subroutine()
+	{
+		int d=0;
+		int s=42/d;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Exc1.subroutine();
+
+	}
+
+}
