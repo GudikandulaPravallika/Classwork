20a18c208db48146045446b9f43a3f21f3f598d9
GudikandulaPravallika <21bq1a4250@vvit.net>
2022-10-04 22:09:53 +0530

    Updated Intefaces file

--- FixedStack.java
+++ FixedStack.java
@@ -4,7 +4,7 @@ public class FixedStack implements IntStack {
 	private int tos;
 	FixedStack(int size)
 	{
-            System.out.println("Intefaces implementation")
+            System.out.println("Intefaces implementation");
 		int stck[]=new int[size];
 		tos=-1;
 	}
